9269f640547c0047bdf69420bdc709cfef5f82c4
Sultan Md. Ayman <32176118+sm-ayman@users.noreply.github.com>
2023-01-29 00:07:41 +0600

    selected items, total price, total shipping, tax and grand total of cart component added

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import './cart.css'
 
-const Cart = ({cart}) => {
+const Cart = ({ cart }) => {
+
+    let total = 0, shipping = 0;
+    for (const product of cart) {
+        total += product.price;
+        shipping += product.shipping;
+    }
+    const tax = (total* 0.1).toFixed(2);
+    const grandTotal = total + shipping + parseFloat(tax);
+
+
     return (
-        <div>
+        <div className='cart'>
             <h4 className='cart-header'>Order Summary</h4>
-            <p>Selected Items: {cart.length}</p> 
+            <p>Selected Items: {cart.length}</p>
+            <p>Total price: ${total} </p>
+            <p>Total Shipping: ${shipping}</p>
+            <p>Tax: ${tax}</p>
+            <h5>Grand Total:  ${grandTotal.toFixed(2)}</h5>
         </div>
     );
 };
